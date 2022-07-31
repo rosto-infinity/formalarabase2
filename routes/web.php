2ebc8e18af56f4aaa083da2e158845b0a2534205
rosto-infinity <rostoinfocus@gmail.com>
2022-07-31 12:12:55 +0200

    infinity..

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Route::get('/posts/create',[PostController::class, 'create'])->name('posts.creat
 Route::post('/posts/create',[PostController::class, 'store'])->name('posts.store');
 
 Route::get('/register', [PostController::class, 'register']);
-
+/**
+ * @param $token
+ */
 Route::get('/posts/{id}',[PostController::class, 'show'])->name('posts.show');
 Route::get('/contact',[PostController::class, 'contact'])->name('conctact');
 
